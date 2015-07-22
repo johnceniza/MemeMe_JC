c9a68066963d296a02c56518a731b5e85bcbaf57
John Ceniza <jc@johnceniza.com>
2015-07-21 22:52:48 -0400

    enabled library picker and loading of image onto image view

--- MemeMe_JC/MemeEditorVC.swift
+++ MemeMe_JC/MemeEditorVC.swift
@@ -8,17 +8,47 @@
 
 import UIKit
 
-class MemeEditorVC: UIViewController {
+class MemeEditorVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+    @IBOutlet weak var memeImageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    //MARK: - UIViewController functions
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK: - IBActions
+    
+    @IBAction func cameraPressed(sender: UIBarButtonItem) {
+        //TODO: add code for camera selection
+    }
+    
+    @IBAction func albumPressed(sender: UIBarButtonItem) {
+        imagePicker.sourceType = .PhotoLibrary
+        imagePicker.delegate = self
+        self.presentViewController(imagePicker, animated: true, completion:nil)
     }
 
+    @IBAction func sharePressed(sender: UIBarButtonItem) {
+        //TODO: add sharing activity view pop up
+    }
+    
+    //MARK: - UIImagePicker delegate functions
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        //user just finished selecting an image - add code to take image and show it on the screen and scale
+        
+        //TODO: add unrwapping and check dict for UIImage type
+        memeImageView.image = image
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
 }
 
