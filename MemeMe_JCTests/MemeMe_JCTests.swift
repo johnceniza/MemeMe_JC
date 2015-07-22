25f9e908eb55115cecb0ae7d827a7e5e72efa39e
John Ceniza <jc@johnceniza.com>
2015-07-21 21:59:58 -0400

    Initial Commit

---
+++ MemeMe_JCTests/MemeMe_JCTests.swift
@@ -0,0 +1,36 @@
+//
+//  MemeMe_JCTests.swift
+//  MemeMe_JCTests
+//
+//  Created by John Ceniza on 7/21/15.
+//  Copyright (c) 2015 AppDeco. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MemeMe_JCTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
