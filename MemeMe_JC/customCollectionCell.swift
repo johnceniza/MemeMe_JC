8e96414c197498b8443dce97ddcc95d88f7d7be0
John Ceniza <jc@johnceniza.com>
2015-08-02 23:37:38 -0400

    collection view functional with segue to detail view

---
+++ MemeMe_JC/customCollectionCell.swift
@@ -0,0 +1,16 @@
+//
+//  customCollectionCell.swift
+//  MemeMe_JC
+//
+//  Created by John Ceniza on 8/2/15.
+//  Copyright (c) 2015 AppDeco. All rights reserved.
+//
+
+import UIKit
+
+class customCollectionCell: UICollectionViewCell {
+
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+}
