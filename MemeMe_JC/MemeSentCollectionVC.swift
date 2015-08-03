da66f2d49b71770876110e78a5c58f0f259c86b0
John Ceniza <jc@johnceniza.com>
2015-08-02 22:52:05 -0400

    added necessary funcs for collectionVC

--- MemeMe_JC/MemeSentCollectionVC.swift
+++ MemeMe_JC/MemeSentCollectionVC.swift
@@ -9,5 +9,26 @@
 import UIKit
 
 class MemeSentCollectionVC: UICollectionViewController {
+
+    var memeArray = [Meme]()
+    
+    override func viewWillAppear(animated: Bool) {
+
+    }
+    
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("collectionCell", forIndexPath: indexPath) as! UICollectionViewCell
+        cell.backgroundColor = UIColor.blackColor()
+        // Configure the cell
+        return cell
+    }
     
 }
